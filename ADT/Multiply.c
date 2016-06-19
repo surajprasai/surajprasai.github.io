016e32fdf785fff6569bd4656e127ffd07caeac6
Suraj Prasai <9841suraj1@gmail.com>
2016-06-19 16:25:12 +0545

    Add files via upload

---
+++ ADT/Multiply.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+
+typedef struct{
+	int numerator;
+	int denominator;
+}rational;
+
+
+rational makerational(int,int);
+rational multiply_rational(rational,rational);
+
+int main()
+{
+		int a,b,c,d;
+	printf("enter numerator of first number: ");
+	scanf("%d",&a);
+	printf("enter denomiantor of first number: ");
+	scanf("%d",&b);
+	printf("enter numerator of second number: ");
+	scanf("%d",&c);
+	printf("enter denominator of second number: ");
+	scanf("%d",&d);
+	
+	rational r1= makerational(a,b);
+	rational r2= makerational(c,d);
+	rational product = multiply_rational(r1,r2);
+	printf("\n\a The product of the two rational number is : %d/%d",product.numerator,product.denominator);
+}
+
+rational makerational(int a, int b)
+{
+	rational r_number;
+	r_number.numerator=a;
+	r_number.denominator=b;
+	if(b==0)
+	{
+		exit(0);
+	}
+	return r_number;
+}
+rational multiply_rational(rational r1,rational r2)
+{
+	rational result;
+	result.numerator=(r1.numerator * r2.numerator);
+	result.denominator=(r1.denominator * r2.denominator);
+	return result;}
